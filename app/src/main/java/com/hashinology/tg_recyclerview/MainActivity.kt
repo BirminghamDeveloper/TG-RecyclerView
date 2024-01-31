dd680335e31e38366c79a2c72076a8e0616e9374
Hashinology <m.sharif.uk@outlook.com>
2024-01-31 09:18:41 +0000

    continues

--- app/src/main/java/com/hashinology/tg_recyclerview/MainActivity.kt
+++ app/src/main/java/com/hashinology/tg_recyclerview/MainActivity.kt
@@ -2,10 +2,14 @@ package com.hashinology.tg_recyclerview
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.LayoutInflater
+import com.hashinology.tg_recyclerview.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    lateinit var viewBinding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        viewBinding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(viewBinding.root)
     }
 }
